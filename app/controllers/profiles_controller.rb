80651361b4627066406387eb36665fe2605332ec
clementbau <bau.clement04@gmail.com>
2026-08-25 15:44:34 +0200

    Added routes and profile controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,39 @@
+class ProfilesController < ApplicationController
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save!
+      redirect_to profile_path(@chat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.update(profile_params)
+    if @profile.save!
+      redirect_to profile_path(@chat)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :age, :location,
+                                    :gender, :username, :description, :language)
+  end
+end
