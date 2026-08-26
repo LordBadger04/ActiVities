834e578537d23399ca329b25ae4d60f50c2da79c
clementbau <bau.clement04@gmail.com>
2026-08-26 12:08:41 +0200

    Added functionning form in new event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,12 +1,13 @@
 class Event < ApplicationRecord
   belongs_to :activity
   belongs_to :user
-  has_many :event_membership, dependent: :destroy
+  has_many :event_memberships, dependent: :destroy
   has_many :chats
   validates :title, presence: true, length: { maximum: 20 }
   validates :location, presence: true
   validates :description, presence: true, length: { minimum: 15 }
-  validates :max_nb_participant, numericality: { greater_than: 0, only_integer: true }
+  validates :max_participant, numericality: { greater_than: 0, only_integer: true }
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :event_date, presence: true
 end
