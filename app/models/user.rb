2096b3507cd59016876532d621e3f0cd786403d6
Florian Durand <florian.grebotdurand@gmail.com>
2026-08-26 16:16:04 +0200

    change userModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  belongs_to :user
+
   has_many :messages
   has_many :events
   has_many :event_memberships
-  belongs_to :profile
+  has_one :profile, dependent: :destroy
   has_many :events_as_participant, through: :event_membership, source: :event
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
