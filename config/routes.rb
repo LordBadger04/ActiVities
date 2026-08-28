8e9acbc4261cdbb24ca614e1067b5983f54ddda0
Florian Durand <florian.grebotdurand@gmail.com>
2026-08-28 10:10:33 +0200

    Corecte the route error

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :events, except: [ :delete ] do
+  resources :events, except: [ :destroy ] do
     member do
       get :cancel
     end
