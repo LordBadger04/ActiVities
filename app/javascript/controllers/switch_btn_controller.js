10d13c2eb8941ea600e3bd8e43ba87121d992e18
Florian Durand <florian.grebotdurand@gmail.com>
2026-08-31 08:49:19 +0200

    Optimized photo displaying

--- app/javascript/controllers/switch_btn_controller.js
+++ app/javascript/controllers/switch_btn_controller.js
@@ -4,33 +4,36 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["movingShape", "leftShape", "rightShape", "eventsComing", "eventsCanceled"]
 
-  connect() {
-    console.log(this.eventsCanceledTargets.value)
-  }
-
   switch(event) {
-    console.log(this.eventsCanceledTargets)
     event.preventDefault();
     if (event.target===this.rightShapeTarget) {
-      this.movingShapeTarget.style.right = "0%";
-      this.leftShapeTarget.classList.remove("switch-selected");
-      this.rightShapeTarget.classList.add("switch-selected");
-      this.eventsComingTargets.forEach(target => {
-        target.classList.add("event-hidden")
-      });
-      this.eventsCanceledTargets.forEach(target => {
-        target.classList.remove("event-hidden")
-      });
+      this.moveRight()
     } else if (event.target===this.leftShapeTarget) {
-      this.movingShapeTarget.style.right = "50%";
-      this.leftShapeTarget.classList.add("switch-selected");
-      this.rightShapeTarget.classList.remove("switch-selected");
-      this.eventsComingTargets.forEach(target => {
-        target.classList.remove("event-hidden")
-      });
-      this.eventsCanceledTargets.forEach(target => {
-        target.classList.add("event-hidden")
-      });
+      this.moveLeft()
+    }
   }
+
+  moveRight() {
+    this.movingShapeTarget.style.right = "0%";
+    this.leftShapeTarget.classList.remove("switch-selected");
+    this.rightShapeTarget.classList.add("switch-selected");
+    this.eventsComingTargets.forEach(target => {
+      target.classList.add("event-hidden")
+    });
+    this.eventsCanceledTargets.forEach(target => {
+      target.classList.remove("event-hidden")
+    });
+  }
+
+  moveLeft() {
+    this.movingShapeTarget.style.right = "50%";
+    this.leftShapeTarget.classList.add("switch-selected");
+    this.rightShapeTarget.classList.remove("switch-selected");
+    this.eventsComingTargets.forEach(target => {
+      target.classList.remove("event-hidden")
+    });
+    this.eventsCanceledTargets.forEach(target => {
+      target.classList.add("event-hidden")
+    });
   }
 }
