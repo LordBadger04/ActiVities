6d9b85007f0e69b3b9e6756c7f7fea4e77200c6c
Florian Durand <florian.grebotdurand@gmail.com>
2026-09-01 17:34:59 +0200

    Added the notification stamp for messages

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,6 +5,7 @@ class ChatsController < ApplicationController
 
   def show
     @chat = Chat.find(params[:id])
+    current_user.notifications.where(chat: @chat).destroy_all
     @message = Message.new
     @event = @chat.event
     @current_user_event_membership = EventMembership.find_by(user: current_user)
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,6 @@
 class Chat < ApplicationRecord
   belongs_to :event
+  has_many :notifications, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :users, through: :messages
 end
